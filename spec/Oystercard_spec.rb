712aa127598d194e850b1d1ea268b49a42428f74
Tim Castillo-Gill <timgillviola@gmail.com>
2020-10-05 16:00:46 +0100

    Created a test and method for balance

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,4 +2,9 @@ require './Oystercard.rb'
 
 describe Oystercard do
 
+  it 'should have a balance of zero' do
+
+    my_oyster_card = Oystercard.new
+    expect(my_oyster_card.balance).to eq 0
+  end
 end
