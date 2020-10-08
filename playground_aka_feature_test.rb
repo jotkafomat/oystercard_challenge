ae4ebd113ba64182f6713df6c450b6c7ae443c36
Dan Holmes <dh@danielholmes.com>
2020-10-08 16:10:56 +0100

    start journey class

--- playground_aka_feature_test.rb
+++ playground_aka_feature_test.rb
@@ -6,11 +6,39 @@ p my_oyster_card = Oystercard.new
 p "this is balance"
 p my_oyster_card.balance
 p my_oyster_card.top_up(20)
-# p my_oyster_card.top_up(100)
-# p my_oyster_card.deduct(2)
+
+# Trip 1
+p my_oyster_card.touch_in("victoria station")
+p my_oyster_card.touch_out("leicester square")
+p my_oyster_card.balance
+p 19
+
+# Trip 2
 p my_oyster_card.touch_in("tulse hill")
-p my_oyster_card.entry_station
+p my_oyster_card.balance
+p 19
 p my_oyster_card.in_journey?
-p my_oyster_card.touch_out("victoria station")
-p my_oyster_card.journey_history
-p victoria_station = Station.new
+p true
+
+# Trip 3
+p my_oyster_card.touch_in("victoria station")
+p my_oyster_card.balance
+p 13
+p my_oyster_card.touch_out("leicester square")
+p my_oyster_card.balance
+p 12
+
+# Trip 4
+p my_oyster_card.touch_out("liverpool street")
+p my_oyster_card.balance
+p 6
+
+p my_oyster_card.journey_history.length
+p 4
+p my_oyster_card.in_journey?
+p false
+
+# p my_oyster_card.touch_out("kings cross")
+# p my_oyster_card.journey_history
+#   # Returns three journies
+# p victoria_station = Station.new
